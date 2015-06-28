9332ec623200022e442ef315f6ffd38cc1010029
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-06-27 22:02:42 -0600

    Dynamically Render Items from State.

--- TODO.md
+++ TODO.md
@@ -2,5 +2,6 @@
 
 + [x] Get something rendered.
 + [x] Make the items more interesting.
++ [x] Render items from state.
 + [ ] Make the items within a list sortable.
 + [ ] Drag items to another list.
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 import SortableList from 'components/SortableList';
 import Item from 'components/Item';
 
+var genId = (function () {
+  var id = 0;
+  return function () {
+    return id++;
+  };
+})();
+
+var state = {
+  items: [
+    { id: genId(), name: 'Item 1' },
+    { id: genId(), name: 'Item 2' },
+    { id: genId(), name: 'Item 3' },
+  ]
+};
+
 React.render(
   <SortableList>
-    <Item>Item1</Item>
-    <Item>Item1</Item>
+    {state.items.map(function (item) {
+      return <Item key={item.id}>{item.name}</Item>
+    })}
   </SortableList>,
   document.body
 );
